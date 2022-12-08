822b11221762b214e96d6c68bcf43503190f6155
kun.ma <makun5201@163.com>
2022-12-08 16:08:43 +0800

    递归的计算方法

--- tools/src/test/java/pers/mk/tools/converter/ToolsApplicationTests.java
+++ tools/src/test/java/pers/mk/tools/converter/ToolsApplicationTests.java
@@ -3,11 +3,38 @@ package pers.mk.tools.converter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 class ToolsApplicationTests {
 
     @Test
     void contextLoads() {
+        BigDecimal c = new BigDecimal(2);
+        List<BigDecimal> cs = new ArrayList<>();
+        cs.add(new BigDecimal("0"));
+        cs.add(new BigDecimal(5572));
+        cs.add(new BigDecimal(5501));
+        cs.add(new BigDecimal(5561));
+        cs.add(new BigDecimal(5541));
+        cs.add(new BigDecimal(5531));
+        BigDecimal recursion = recursion(cs, 5);
+        System.out.println(recursion);
+    }
+
+    private BigDecimal recursion(List<BigDecimal> cs, Integer n){
+        BigDecimal fz1 = cs.get(n).multiply(new BigDecimal(2));
+        if (n == 0){
+            return new BigDecimal(0);
+        }
+        BigDecimal fz2 = new BigDecimal(n - 1).multiply(this.recursion(cs, n - 1));
+        BigDecimal fm = new BigDecimal(n + 1);
+        BigDecimal fz = fz1.add(fz2);
+        BigDecimal divide = fz.divide(fm,2, RoundingMode.HALF_UP);
+        return divide;
     }
 
 }
